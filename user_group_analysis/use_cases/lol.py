7af62a9b86b1cf2d21d9266f2db61d66cac3350f
Arnjee <arnist91@gmail.com>
2020-11-25 09:38:20 +0000

    Dagbókar fræsla

---
+++ user_group_analysis/use_cases/lol.py
@@ -0,0 +1,11 @@
+import os
+def clear_sceen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+print("hey")
+lol = input("input")
+clear_sceen()
+print("YO!")
+hey = input("")
+clear_sceen()
+print("virkar")
