e67d1583368790c0273228de9e156d90c6850a03
thorrafnar <thorsteinningis@gmail.com>
2020-11-30 21:07:14 +0000

    main pls

--- project/ui/ui_main.py
+++ project/ui/ui_main.py
@@ -2,20 +2,18 @@ from project.logic.logicAPI import LogicAPI
 
 class UI_Main():
     def __init__(self):
-        self.logicAPI = LogicAPI()
+        self.logic_api = LogicAPI()
         
 
     def start(self):
         print("Hi there UI")
 
     def get_employees(self):
-        employees = self.logicAPI.get_employees()
+        employees = self.logic_api.get_employees()
         for employee in employees:
             print(employee.name)
         
     def get_filtered_employees(self, attribute_list):
-        employees = self.logicAPI.get_filtered_employees(attribute_list)
+        employees = self.logic_api.get_filtered_employees(attribute_list)
         for emp in employees:
-            print(emp.name)
-
-    
+            print(emp.name)   
