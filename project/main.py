837e6c3e92f3ad6da11f3fe8c7e33c83f59bf688
thorrafnar <thorsteinningis@gmail.com>
2020-11-30 10:30:53 +0000

    Fixed#

--- project/main.py
+++ project/main.py
@@ -1,10 +1,12 @@
 while True:
     grade = input("Enter total grade for the group: ")
     try:
-        grade = int(grade)
-        if grade > 9:
+        grade = float(grade)
+        if grade < 9:
             print("Wrong grade")
         else:
             break
+    except ValueError:
+        print("Enter a number")
 
 print("Group got a grade of", grade)
