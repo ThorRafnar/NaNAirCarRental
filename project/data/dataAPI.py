c13ff4b8d326f3c327f576db694ac12a2f50d1ff
AAR <aar@aar.is>
2020-12-01 09:08:31 +0000

    removed project folder

--- project/data/dataAPI.py
+++
@@ -1,9 +0,0 @@
-from project.data.employee_data import EmployeeData
-
-class DataAPI():
-    def __init__(self):
-        print("Hello data layer! :~)")
-        self.employee_data = EmployeeData()
-
-    def get_employees(self):
-        return self.employee_data.get_employees()
