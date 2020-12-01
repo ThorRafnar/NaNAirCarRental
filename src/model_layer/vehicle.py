f91685962e7c2cc82a7a990f5e11a9f77f2dd716
Ragnar <ragnaro20@ru.is>
2020-12-01 12:10:14 +0000

    Made changes to data_layer

---
+++ src/model_layer/vehicle.py
@@ -0,0 +1,11 @@
+
+class VehicleLogic:
+    def __init__(self, manufacturer, model, type, status, man_year, color, license_type, location):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.type = status
+        self.status = status
+        self.man_year = man_year
+        self.color = color
+        self.license_type = license_type
+        self.location = location
