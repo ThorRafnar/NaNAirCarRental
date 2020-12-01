9aadff83717e9c27b3f64521381e0160bd785bc6
Arnjee <arnist91@gmail.com>
2020-12-01 15:35:53 +0000

    db func added

--- src/data_layer/employee_data.py
+++ src/data_layer/employee_data.py
@@ -6,11 +6,21 @@ class EmployeeData():
 
     def get_employees(self):
         ret_list = []
-        print(os.getcwd())
         with open("data_layer/data_files/employee.csv", encoding="utf-8") as file_stream:
             reader = csv.DictReader(file_stream)
             for row in reader:
                 emp = Employee(row["name"], row["address"], row["postal_code"], row["ssn"], row["home_phone"], row["mobile_phone"], row["email"], row["work_area"])
                 ret_list.append(emp)
-                
         return ret_list 
+
+
+    def register_employee(self,emp):
+        ''' Writes new employee to database'''
+        a_list = [emp.name,emp.address,emp.postal_code,emp.ssn,emp.home_phone,emp.mobile_phone,emp.email,emp.work_area]
+        with open("data_layer/data_files/employee.csv", 'a+',encoding="utf8",newline='') as file_stream:
+            csvwriter = csv.writer(file_stream)
+            csvwriter.writerow(a_list)
+        
+
+            
+        
