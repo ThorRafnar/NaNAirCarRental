c30a8cc938bf2d8e16a0047ee1d9a0fa7d9a6f86
AAR <aar@aar.is>
2020-12-01 13:11:05 +0000

    adding check_employee and register_employee in logic layer

--- src/logic_layer/employee_logic.py
+++ src/logic_layer/employee_logic.py
@@ -22,3 +22,18 @@ class EmployeeLogic():
                 ret_list.append(emp)
 
         return ret_list
+
+    def check_employee(self, new_emp):
+        '''  '''
+        emp_list = self.get_employees()
+        is_new = True
+        for emp in emp_list:
+            if emp.ssn == new_emp.ssn:
+                is_new = False
+                break
+        print(is_new)
+        return is_new
+
+    def register_employee(self, emp):
+        # þarf að fá register_employee frá data api
+        self.data_api.register_employee(emp)
