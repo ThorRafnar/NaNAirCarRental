e2151db46b7540fdc98234476d906f3fa966a943
Arnjee <arnist91@gmail.com>
2020-12-02 09:37:53 +0000

    change_employee_info fuction added to db

--- src/data_layer/arni_test.py
+++ src/data_layer/arni_test.py
@@ -17,47 +17,19 @@ class Employee():
         self.work_area = work_area
 
 
-def change_employee_info(att_list):
-    ''' Takes in att_list :^)  '''
-    with open("testcsv.csv",'r' ,encoding="utf-8",) as read_file:
-        reader = csv.DictReader(read_file)
-        file_list = []
-        for row in reader:
-            if row["ssn"] == att_list[0]:
-                key = att_list[1]
-                new = att_list[2]
-                row[key] = new
-            file_list.append(row)
+def get_employees():
+        ret_list = []
+        with open("testcsv.csv", encoding="utf-8") as file_stream:
+            reader = csv.DictReader(file_stream)
+            for row in reader:
+                emp = Employee(row["name"], row["address"], row["postal_code"], row["ssn"], row["home_phone"], row["mobile_phone"], row["email"], row["work_area"])
+                ret_list.append(emp)
+            for el in ret_list:
+                print(el.ssn)
         
-    with open("testcsv.csv", "w", encoding="utf-8", newline='') as write_file:
-        keys = file_list[0].keys()
-        the_writer = csv.DictWriter(write_file,keys)
-        the_writer.writeheader()
-        the_writer.writerows(file_list)
+        return ret_list 
 
-
-
-
-
-
-# def change_employee_info(att_list):
-#     ''' Takes in att_list  '''
-#     with open("testcsv.csv",'r' ,encoding="utf-8",) as read_file:
-#         reader = csv.DictReader(read_file)
-#         for row in reader:
-#             if row["ssn"] == att_list[0]:
-#                 emp = Employee(row["name"], row["address"], row["postal_code"], row["ssn"], 
-#                 row["home_phone"], row["mobile_phone"], row["email"], row["work_area"])
-#                 key = att_list[1]
-#                 new = att_list[2]
-#                 #Sets emp.key as new, changes the attribute
-#                 setattr(emp, key, new)
-#         write
-
-
-
-            
-    
+get_employees()
         
 
 
@@ -67,7 +39,3 @@ def change_employee_info(att_list):
             
            
         
-change_employee_info(att)
-att = ["300279-1289","address","Vestursíða 38"]
-
-
