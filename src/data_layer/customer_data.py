fa2c9de691b42b5ead154bb85352b48f7e734311
Arnjee <arnist91@gmail.com>
2020-12-03 15:22:48 +0000

    change_emoloyee fór ekki í gegn, gleymdi að savea :^)

--- src/data_layer/customer_data.py
+++ src/data_layer/customer_data.py
@@ -22,19 +22,19 @@ class CustomerData():
     def change_customer_info(self, att_list):
         '''Takes in a list with attributes. Uses thous attributes to 
         change certain info about the customer '''
-        with open("data_layer/data_files/customers.csv", "r", encoding='utf-8') as read_file:
+        values = a_list[1].split(",")
+        file_list = []
+        with open("test.csv",'r' ,encoding="utf-8") as read_file:
             reader = csv.DictReader(read_file)
-            file_list = []
+            emp_ssn = a_list[0]
             for row in reader:
-                # Key is the key for the attribute we want to change, and new is the new attribute
-                if row["ssn"] == att_list[0]:
-                    key = att_list[1]
-                    new = att_list[2]
-                    row[key] = new
+                if row["ssn"] == emp_ssn:
+                    for key, value in zip(row, values):
+                        row[key] = value
                 file_list.append(row)
-                
-        with open("data_layer/data_files/customers.csv", "w", encoding='utf-8', newline='') as write_file:
+
+        with open("test.csv", "w", encoding='utf-8', newline='') as write_file:
             keys = file_list[0].keys()
-            writer = csv.DictWriter(write_file,keys)
-            writer.writeheader()
-            writer.writerows(file_list)
+            the_writer = csv.DictWriter(write_file,keys)
+            the_writer.writeheader()
+            the_writer.writerows(file_list)
