f0dc79b646e2e53518594b078bbcb3b1fedc5855
AAR <aar@aar.is>
2020-12-04 12:49:39 +0000

    Added methods to vehicle logic, new functionality for register vehicle and find vehicle methods

--- src/logic_test.py
+++ src/logic_test.py
@@ -4,5 +4,6 @@ from model_layer.vehicle import Vehicle
 
 r = LogicAPI()
 #emp = Employee('Jón Jónsson','Melagerð 99','200','300279-1289','+356 5815432','+354 6890012','jj@nan.is','KEF')
-veh = Vehicle('Yedoo','Trexx','Light road','OK','2020','Orange',None,'Longyearbyen',None)
-dest = r.register_new_vehicle(veh)
+veh = Vehicle('Yedoo','Trexx','Light road','OK','2020','Orange',None,'KUS',None)
+dest = r.find_vehicle('1')
+print(dest)
