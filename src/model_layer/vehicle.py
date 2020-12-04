c556560865a17d1fd4b472b7d0102c4236c406ed
thorrafnar <thorsteinningis@gmail.com>
2020-12-04 11:21:43 +0000

    Model changes mostly

--- src/model_layer/vehicle.py
+++ src/model_layer/vehicle.py
@@ -1,11 +1,12 @@
 
 class Vehicle:
-    def __init__(self, manufacturer, model, vehicle_type, status, man_year, color, licence_type, location):
+    def __init__(self, manufacturer, model, vehicle_type, status, man_year, color, licence_type, location, id_number):
         self.manufacturer = manufacturer
         self.model = model
         self.type = vehicle_type
         self.status = status
-        self.man_year = man_year
+        self.year = man_year
         self.color = color
         self.licence_type = licence_type
         self.location = location
+        self.id = id_number
