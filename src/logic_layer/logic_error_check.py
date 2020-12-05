8a6aaf9fd2c60cf2d6145ff214cb38242dd5ac2f
Ragnar <ragnaro20@ru.is>
2020-12-05 13:17:54 +0000

    Added functions to logic_error_check.py

--- src/logic_layer/logic_error_check.py
+++ src/logic_layer/logic_error_check.py
@@ -56,3 +56,69 @@ class LogicErrorCheck():
             return None
         
         return a_str
+    
+    #This function can be used whenever a string has to be only numbers with no spaces
+    def check_if_number(self, a_str):
+        try:
+            numb = int(a_str)
+            return a_str
+        except ValueError:
+            return None
+    
+    def check_valid_vehicle_type(self, a_str):
+        all_vehicle_types = self.data_api.get_all_vehicle_types()
+        valid_types = [getattr(name, "name") for name in all_vehicle_types]
+        for vehicle_type in valid_types:
+            if str(vehicle_type.lower()) == a_str.lower():
+                return a_str
+
+        return None
+    
+    def check_location(self, a_str):
+        all_airports = self.data_api.get_destinations()
+        valid_airports = [getattr(airport, "airport") for airport in all_airports]
+        for airport in valid_airports:
+            if str(airport.lower()) == a_str.lower():
+                return a_str
+        return None
+    
+    def check_color(self, a_str):
+        # These are the valid colors for the system
+        valid_colors = ["blue","green","purple","grey","black","white","orange","yellow","pink","brown"]
+        if a_str.lower() in valid_colors:
+            return a_str.lower()
+        else:
+            return None
+    
+    def check_ssn_inpunt(self, a_str):
+        ''' Takes in a string that is supposed to be social security number 
+        and checks if it is valid or not.'''
+        new_str = ""
+        final_str = ""
+        for char in a_str:
+            if char.isspace():
+                pass
+            new_str += char
+        
+        if len(new_str) > 10:
+            return None
+        
+        try:
+            numb = int(new_str)
+        except ValueError:
+            return None
+        
+        birtday = new_str[0:6]
+        if (int(new_str[0:2]) > 31) or (int(new_str[2:4]) > 12):
+            return None
+        
+        counter = 0
+        for char in new_str:
+            counter += 1
+            final_str += char
+            if counter == 6:
+                final_str += "-"
+        
+        return final_str
+
+    
