9ac51ce79002eafdec1b2862a2b671f546043f23
AAR <aar@aar.is>
2020-12-06 17:55:56 +0000

    Helgarvinna í contract logic, fullt af virkni komin fyrir contract

--- src/logic_layer/contract_logic.py
+++ src/logic_layer/contract_logic.py
@@ -1,13 +1,29 @@
+from datetime import date
+
 class ContractLogic():
-    def __init__(self, data_api):
+    def __init__(self, data_api, vehicle_logic, type_logic):
         self.data_api = data_api
+        self.vehicle_logic = vehicle_logic
+        self.type_logic = type_logic
 
     def get_all_contracts(self):
-        return self.list_all_contracts()
+        return self.data_api.list_all_contracts()
 
     def create_new_contract(self, contract):
+        contract.base_price = self.set_contract_base_price(contract)
+        contract.contract_id = self.set_contract_id()
         self.data_api.new_contract(contract)
     
+    def set_contract_base_price(self, contract):
+        vehicle = self.vehicle_logic.find_vehicle(contract.vehicle_id)
+        type_rate = self.type_logic.get_types_rate(vehicle.type)
+        number_of_days = self.calculate_number_of_days(contract.loan_date, contract.end_date)
+        return type_rate * number_of_days
+    
+    def set_contract_id(self):
+        contr_list = self.get_all_contracts()
+        return len(contr_list) + 1
+        
     def view_customer_contracts(self, ssn):
         contracts_list = self.get_all_contracts()
         customer_contracts = []
@@ -25,7 +41,35 @@ class ContractLogic():
         return correct_contract
     
     def change_contract_status(self, contract_id, status):
-        self.data_api.change_contract_status(contract_id, status)
+        day_fine = 200
+        cont = self.find_contract(contract_id)
+        cont.status = status
+        if status == 'returned':
+            cont.return_date = date.today().strftime('%d/%m/%Y')
+            self.vehicle_logic.change_vehicle_condition(cont.vehicle_id, 'rentable')
+            date_status = self.calculate_number_of_days(cont.end_date, cont.return_date)
+            if date_status > 0:
+                cont.extensions = day_fine * date_status
+            cont.total = self.get_contract_total_price(cont)
+        elif status == 'paid':
+            pass
+        else:
+            pass
+
+        attr_str = f'{cont.contract_id},{cont.customer_ssn},{cont.employee_ssn},{cont.vehicle_id},{cont.loan_date},{cont.end_date},{cont.base_price},{cont.return_date},{cont.extensions},{cont.total},{cont.status}'
+        attr_list = [cont.contract_id, attr_str]
+        self.data_api.change_contract_status(attr_list)
+    
+    def calculate_number_of_days(self, start_date, end_date):
+        d,m,y = int(start_date[:2]),int(start_date[3:5]),int(start_date[6:])
+        e_d,e_m,e_y = int(end_date[:2]),int(end_date[3:5]),int(end_date[6:])
+        f_date = date(y, m, d)
+        l_date = date(e_y, e_m, e_d)
+        delta = l_date - f_date
+        return delta.days
+    
+    def get_contract_total_price(self, contract):
+        return int(contract.base_price) + int(contract.extensions)
 
     def get_contracts_by_attr(self, attr_list):
         col = attr_list[0]
--- src/logic_layer/vehicle_type_logic.py
+++ src/logic_layer/vehicle_type_logic.py
@@ -10,6 +10,15 @@ class VehicleTypeLogic():
 
     def change_types_rate(self, type_name, new_rate):
         self.data_api.change_vehicle_rate(new_rate, type_name)
+    
+    def get_types_rate(self, selected_type):
+        types_list = self.data_api.get_all_vehicle_types()
+        rate = 0
+        for t in types_list:
+            if t.name.lower() == selected_type.lower():
+                rate = int(t.rate)
+                break
+        return rate
 
     def filter_by_region(self, reg):
         types_list = self.data_api.get_all_vehicle_types()
