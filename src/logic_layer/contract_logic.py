53efa5f6e89d1ddc063d9fabc95ad0f3c5ad952c
AAR <aar@aar.is>
2020-12-07 09:38:36 +0000

    changes to contract logic, calculate extensions and more

--- src/logic_layer/contract_logic.py
+++ src/logic_layer/contract_logic.py
@@ -7,33 +7,40 @@ class ContractLogic():
         self.type_logic = type_logic
 
     def get_all_contracts(self):
+        ''' Returns a list of all contracts as instances of Contract classes '''
         return self.data_api.list_all_contracts()
 
     def create_new_contract(self, contract):
+        ''' Does initial operations on new contract like calculating base price, reserving given vehicle and giving contract new ID, than sending that contract to data layer to store in contract.csv '''
         contract.base_price = self.set_contract_base_price(contract)
         contract.contract_id = self.set_contract_id()
         self.vehicle_logic.change_vehicle_condition(contract.vehicle_id, 'in_rent')
         self.data_api.new_contract(contract)
     
     def set_contract_base_price(self, contract):
+        ''' Calculates base price from vehicle type rate multiplied by number of days rented '''
         vehicle = self.vehicle_logic.find_vehicle(contract.vehicle_id)
         type_rate = self.type_logic.get_types_rate(vehicle.type)
         number_of_days = self.calculate_number_of_days(contract.loan_date, contract.end_date)
         return type_rate * number_of_days
     
     def set_contract_id(self):
+        ''' Adds new ID on new contract '''
         contr_list = self.get_all_contracts()
         return len(contr_list) + 1
+        # Breyta aðeins
         
     def view_customer_contracts(self, ssn):
+        ''' Gets an customer ssn from UI and checks if customer owns contracts in database, returns a list of all contracst for given ssn if found, else returns an empty list '''
         contracts_list = self.get_all_contracts()
         customer_contracts = []
         for contract in contracts_list:
-            if contract.ssn == ssn:
+            if contract.customer_ssn == ssn:
                 customer_contracts.append(contract)
         return customer_contracts
     
     def find_contract(self, cont_id):
+        ''' Searches for a contract from given contract ID and returns an instance of contract class if found, else returns None '''
         contracts_list = self.get_all_contracts()
         correct_contract = None
         for contract in contracts_list:
@@ -42,26 +49,27 @@ class ContractLogic():
         return correct_contract
     
     def change_contract_status(self, contract_id, status):
-        day_fine = 200
+        ''' Changes contract status for given contract by ID '''
         cont = self.find_contract(contract_id)
-        cont.status = status
-        if status == 'returned':
+        cont.status = status.lower()
+        if cont.status == 'returned':
             cont.return_date = date.today().strftime('%d/%m/%Y')
             self.vehicle_logic.change_vehicle_condition(cont.vehicle_id, 'rentable')
             date_status = self.calculate_number_of_days(cont.end_date, cont.return_date)
             if date_status > 0:
-                cont.extensions = day_fine * date_status
+                cont.extensions = self.calc_extensions(cont.vehicle_id, date_status)
             cont.total = self.get_contract_total_price(cont)
-        elif status == 'paid':
-            pass
-        else:
-            pass
-
+        elif cont.status == 'paid':
+            paid_date = date.today().strftime('%d/%m/%Y')
+            profit_log = [paid_date,cont.contract_id,cont.base_price,cont.extensions,cont.total]
+            self.data_api.create_profit_log(profit_log)
+            
         attr_str = f'{cont.contract_id},{cont.customer_ssn},{cont.employee_ssn},{cont.vehicle_id},{cont.loan_date},{cont.end_date},{cont.base_price},{cont.return_date},{cont.extensions},{cont.total},{cont.status}'
         attr_list = [cont.contract_id, attr_str]
         self.data_api.change_contract_status(attr_list)
     
     def calculate_number_of_days(self, start_date, end_date):
+        ''' Calculates how many days are between given dates '''
         d,m,y = int(start_date[:2]),int(start_date[3:5]),int(start_date[6:])
         e_d,e_m,e_y = int(end_date[:2]),int(end_date[3:5]),int(end_date[6:])
         f_date = date(y, m, d)
@@ -69,13 +77,21 @@ class ContractLogic():
         delta = l_date - f_date
         return delta.days
     
+    def calc_extensions(self, vehicle_id, date_status):
+        vehicle = self.vehicle_logic.find_vehicle(vehicle_id)
+        type_rate = int(self.type_logic.get_types_rate(vehicle.type))
+        tax = type_rate * 1.5
+        return date_status * tax
+
+    
     def get_contract_total_price(self, contract):
+        ''' Adds extensions with base price to calculate total price '''
         return int(contract.base_price) + int(contract.extensions)
 
     def get_contracts_by_attr(self, attr_list):
+        ''' Returns a list of contracts from given attributes '''
         col = attr_list[0]
         value = attr_list[1]
         contracts_list = self.get_all_contracts()
         filtered_list = [c for c in contracts_list if getattr(c, col).lower() == value.lower()]
         return filtered_list
-        
