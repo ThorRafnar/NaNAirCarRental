bf9960dc2e8dad0486aa60c5f376582d364073c6
Arnjee <arnist91@gmail.com>
2020-12-11 10:51:13 +0000

    database commented

--- src/data_layer/chuck_data.py
+++ src/data_layer/chuck_data.py
@@ -1,5 +1,6 @@
 class ChuckData():
     def get_jokes(self):
+        ''' returns a list with all the chuck jokes '''
         with open('data_layer/data_files/chuck_jokes.txt', 'r', encoding='utf-8', newline='') as file_stream:
             jokes_list = []
             for row in file_stream:
